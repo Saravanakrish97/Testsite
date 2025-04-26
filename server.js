b51caee5b9fee771a9cb22768c408e6499a48e6b
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2025-04-27 00:39:57 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.send("Chat App Server is Running!");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port 3000");
+});
