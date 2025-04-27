64ab15ecec015e59dd620be01620b8b52e4857c2
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2025-04-27 10:19:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,27 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-// Serve static files from "public" folder (or wherever your HTML files are)
 app.use(express.static('public'));
 
-// Default route (optional)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// Socket.io connection
 io.on('connection', (socket) => {
     console.log('A user connected');
-    // your chat code here
+
+    socket.broadcast.emit('chat message', 'A new user has joined the chat');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
-// Start server
 const port = process.env.PORT || 3000;
 http.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
